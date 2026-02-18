938e8318f2a63a88cd245c406eeca09e0589e775
smycynek <sv@stevenvictor.net>
2026-02-18 17:30:22 -0500

    info box

--- src/App.tsx
+++ src/App.tsx
@@ -24,6 +24,7 @@ function App() {
   let playIntervalId: number | undefined;
   let fileInputRef: HTMLInputElement | undefined;
   const [showAbout, setShowAbout] = createSignal(false);
+  const [showGameInfo, setShowGameInfo] = createSignal(false);
   const [showScore, setShowScore] = createSignal(false);
   let scoreTimeout: ReturnType<typeof setTimeout> | null = null;
 
@@ -187,7 +188,12 @@ function App() {
       <div class="header-row">
         <h1>FAMOUS GAMES 3D</h1>
         <div class="controls">
-          <select id="game-select" onChange={handleGameSelect} value={selectedGame()?.name}>
+          <select
+            title="Select a game"
+            id="game-select"
+            onChange={handleGameSelect}
+            value={selectedGame()?.name}
+          >
             <For each={gameList()}>
               {(game) => (
                 <option value={game.name}>
@@ -197,12 +203,15 @@ function App() {
               )}
             </For>
           </select>
+          <button class="info-btn" onClick={() => setShowGameInfo(true)} title="Game Info">
+            <img src={infoIcon} alt="Game Info" class="button-icon" />
+          </button>
           <button class="upload-btn" onClick={handleUploadPgn} title="Upload a PGN">
             <img src={uploadIcon} alt="Upload a PGN" class="button-icon" />
           </button>
           <input
             type="file"
-            accept=".pgn"
+            accept="text/pgn"
             ref={fileInputRef}
             onChange={handleFileChange}
             style={{ display: 'none' }}
@@ -232,15 +241,16 @@ function App() {
           <span class="turn-icon">
             {!isPlaying() && moveIndex() < getTotalMoves() - 1 && (
               <img
+                title={(moveIndex() + 1) % 2 === 0 ? 'White to move' : 'Black to move'}
                 src={(moveIndex() + 1) % 2 === 0 ? whiteKingIcon : blackKingIcon}
                 alt={(moveIndex() + 1) % 2 === 0 ? 'White to move' : 'Black to move'}
               />
             )}
           </span>
-          <span class="move-counter">
+          <span title="Current move" class="move-counter">
             {String(moveIndex() + 1).padStart(3, '0')}/{String(getTotalMoves()).padStart(3, '0')}
           </span>
-          <span class="score">
+          <span title="Game result">
             {showScore() ? selectedGame()?.parsed[0]?.tags?.Result || '----' : '----'}
           </span>
         </div>
@@ -258,19 +268,52 @@ function App() {
           <img src={infoIcon} alt="About" class="button-icon" />
         </button>
       </div>
+      {showGameInfo() && (
+        <div class="about-overlay" onClick={() => setShowGameInfo(false)}>
+          <div class="game-info-dialog" onClick={() => setShowGameInfo(false)}>
+            <h1 class="game-info-title">GAME INFO</h1>
+            <p>
+              <strong>White:</strong> {selectedGame()?.parsed[0]?.tags?.White || 'Unknown'}
+            </p>
+            <p>
+              <strong>Black:</strong> {selectedGame()?.parsed[0]?.tags?.Black || 'Unknown'}
+            </p>
+            <p>
+              <strong>Event:</strong> {selectedGame()?.parsed[0]?.tags?.Event || 'Unknown'}
+            </p>
+            <p>
+              <strong>Site:</strong> {selectedGame()?.parsed[0]?.tags?.Site || 'Unknown'}
+            </p>
+            <p>
+              <strong>Date:</strong>{' '}
+              {selectedGame()?.parsed[0]?.tags?.Date?.value ||
+                selectedGame()?.parsed[0]?.tags?.Date ||
+                'Unknown'}
+            </p>
+            <p>
+              <strong>Winner:</strong>{' '}
+              {selectedGame()?.parsed[0]?.tags?.Result === '1-0'
+                ? 'White'
+                : selectedGame()?.parsed[0]?.tags?.Result === '0-1'
+                  ? 'Black'
+                  : selectedGame()?.parsed[0]?.tags?.Result === '1/2-1/2'
+                    ? 'Draw'
+                    : 'Unknown'}
+            </p>
+          </div>
+        </div>
+      )}
       {showAbout() && (
         <div class="about-overlay" onClick={() => setShowAbout(false)}>
           <div class="about-dialog" onClick={(e) => e.stopPropagation()}>
             <h1 class="about-title">ABOUT FAMOUS GAMES 3D</h1>
-            <p>
+            <p class="about-content">
               Play back famous chess games from history, or upload your own{' '}
               <a href="https://en.wikipedia.org/wiki/Portable_Game_Notation">PGN</a> files to
-              visualize them in 3D! Pieces modeled in{' '}
+              visualize them in 3D! (Desktop only) Pieces modeled in{' '}
               <a href="https://cad.onshape.com/documents/1ac43c0042a8a0544e84feed/w/276b025152b1f726b298cef5/e/c3193025dfaf1a651f190a93">
                 Onshape.
-              </a>
-            </p>
-            <p>
+              </a>{' '}
               Gameplay in <a href="https://www.solidjs.com/">SolidJS</a>,{' '}
               <a href="https://threejs.org/">ThreeJS</a>, and{' '}
               <a href="https://www.npmjs.com/package/chess.js">chess.js.</a> PGN-parsing with{' '}
@@ -281,6 +324,12 @@ function App() {
             <a href="https://github.com/smycynek/famousgames3d-v4" class="credits">
               https://github.com/smycynek/famousgames3d-v4
             </a>
+
+            <div class="playback-controls">
+              <button class="ok" onClick={() => setShowAbout(false)}>
+                OK
+              </button>
+            </div>
           </div>
         </div>
       )}
